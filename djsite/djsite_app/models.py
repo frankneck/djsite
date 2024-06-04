f0a2aea34ad564a3e30f8edf0c660a1cc255009e
finnf <finn.f1rst128@gmail.com>
2024-06-04 18:25:40 +1000

    created model 'Game'

--- djsite/djsite_app/models.py
+++ djsite/djsite_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    category = models.CharField(max_length=100)
+    vendor = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.IntegerField()
+    number_of_players = models.CharField(max_length=10)
+    age = models.CharField(max_length=10)
