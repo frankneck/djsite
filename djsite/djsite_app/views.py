c2d8453abbd19c4cf8762a8f7a75cb648419535a
finnf <finn.f1rst128@gmail.com>
2024-06-05 12:28:09 +1000

    created templates, static

--- djsite/djsite_app/views.py
+++ djsite/djsite_app/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
 
+menu = ['О сайте', 'Рассылка', 'Вход', 'Предложить идею']
+
 
 def index(request):
     information = Game.objects.all()
-    return render(request, 'djsite_app/index.html', {'title': 'Главная страница', 'information': information} )
+    return render(request, 'djsite_app/index.html', {'title': 'Главная страница', 'information': information, 'menu': menu})
 
 
 def pageNotFound(request, exception):
